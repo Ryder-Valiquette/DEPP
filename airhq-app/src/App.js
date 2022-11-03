491cd6be88d71d20a536f7c9fb964ef25d4fb310
Larissa Pearce <larissa.pearce@ufl.edu>
2022-11-02 21:54:10 -0400

    Using Bootstrap now

---
+++ airhq-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "react-bootstrap/Button"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Test</Button> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
