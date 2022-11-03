dc49a3f307a67ea42290b149ad70e81056856d82
Larissa Pearce <larissa.pearce@ufl.edu>
2022-11-02 22:23:04 -0400

    Testing Bootstrap

--- airhq-app/src/App.js
+++ airhq-app/src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from "react-bootstrap/Button"
 import "bootstrap/dist/css/bootstrap.min.css"
+
+import { Button, Alert, Breadcrumb, BreadcrumbItem, Card, CardImg } from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Card>
+          <Card.Img src="https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" />
+          <Card.Body>
+            <Card.Title style={{color: "#000"}}>
+              Card Example
+            </Card.Title>
+            <Card.Text style={{color: "#000"}}>
+              Cards example
+            </Card.Text>
+            <Button variant="primary">Button</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <BreadcrumbItem>Test1</BreadcrumbItem>
+          <BreadcrumbItem>Test2</BreadcrumbItem>
+          <BreadcrumbItem>Test3</BreadcrumbItem>
+        </Breadcrumb>
+        <Alert variant="success">only dummies can read this</Alert>
         <Button>Test</Button> 
       </header>
     </div>
