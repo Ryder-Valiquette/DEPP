a834dce276786ec678ed294ac0792a3dc53e53dc
vjc22 <73601189+vjc22@users.noreply.github.com>
2022-12-04 16:35:08 -0500

    Update Education.js
    commenting

--- airhq-app/src/components/Education.js
+++ airhq-app/src/components/Education.js
@@ -5,13 +5,17 @@ import './Education.css';
 
 import AQHeart from "../assets/AQHeart.png";
 import AQRange from "../assets/AQRange.png";
-let value = "";
-let i = 0;
+
+let value = 0;
 export default class Education extends Component {
   render() {
+
+  //This function handles the dropdown menu selection and displays the corresponding information on the page
   function handleSelect(e){
     console.log(e);
     value = e;
+
+    //This if statement handles displaying the pollution information
     if (e === "option-1"){
       
       document.getElementById("Common Pollutant Information").innerHTML = "Pollutants Information<br>";
@@ -25,6 +29,8 @@ export default class Education extends Component {
       document.getElementById("sulfur").innerHTML = "Sulfur Info<br>";
       document.getElementById("voc").innerHTML = "Volatile Organic Compound Info<br>";
 
+
+      //This block of code erases the community information from the page if it is already displayed
       document.getElementById("Community").innerHTML = "";
       document.getElementById("schools").innerHTML = "";
       document.getElementById("communities").innerHTML = "";
@@ -33,14 +39,17 @@ export default class Education extends Component {
       document.getElementById("you").innerHTML = "";
 
 
-
+      //This block of code erases the health information from the page if it is already displayed
       document.getElementById("Health").innerHTML = "";
       document.getElementById("Exposure").innerHTML = "";
       document.getElementById("Indoor").innerHTML = "";
       document.getElementById("Indoorold").innerHTML = "";
       document.getElementById("Indoorchild").innerHTML = "";
     }
+    
+    //This if statement handles displaying the community information
     if (e === "option-2"){
+      //This block of code erases the pollutant information from the page if it is already displayed
       document.getElementById("Common Pollutant Information").innerHTML = "";
       document.getElementById("ozone").innerHTML = "";
       document.getElementById("pm").innerHTML = "";
@@ -52,6 +61,7 @@ export default class Education extends Component {
       document.getElementById("sulfur").innerHTML = "";
       document.getElementById("voc").innerHTML = "";
 
+      //This block of code displays the community information on the page
       document.getElementById("Community").innerHTML = "Community<br>";
       document.getElementById("schools").innerHTML = "Take Action to Improve Indoor Air Quality in Schools<br>";
       document.getElementById("communities").innerHTML = "Poor Communities Exposed to Elevated Air Pollution Levels<br>";
@@ -60,7 +70,7 @@ export default class Education extends Component {
       document.getElementById("you").innerHTML = "What you can do about air pollution<br>";
       
 
-
+      //This block of code erases the health information from the page if it is already displayed
       document.getElementById("Health").innerHTML = "";
       document.getElementById("Exposure").innerHTML = "";
       document.getElementById("Indoor").innerHTML = "";
@@ -69,7 +79,10 @@ export default class Education extends Component {
       document.getElementById("research").innerHTML = "";
       document.getElementById("10").innerHTML = "";
     }
+
+    //This if statement handles displaying the health information
     if (e === "option-3"){
+      //This block of code erases the pollutant information from the page if it is already displayed
       document.getElementById("Common Pollutant Information").innerHTML = "";
       document.getElementById("ozone").innerHTML = "";
       document.getElementById("pm").innerHTML = "";
@@ -81,6 +94,7 @@ export default class Education extends Component {
       document.getElementById("sulfur").innerHTML = "";
       document.getElementById("voc").innerHTML = "";
 
+      //This block of code erases the community information from the page if it is already displayed
       document.getElementById("Community").innerHTML = "";
       document.getElementById("schools").innerHTML = "";
       document.getElementById("communities").innerHTML = "";
@@ -88,7 +102,7 @@ export default class Education extends Component {
       document.getElementById("justice").innerHTML = "";
       document.getElementById("you").innerHTML = "";
 
-      
+      //This block of code displays the health information on the page
       document.getElementById("Health").innerHTML = "Health<br>";
       document.getElementById("Exposure").innerHTML = "Air Pollution Exposure and Health Effects<br>";
       document.getElementById("Indoor").innerHTML = "Indoor Air Quality Info<br>";
@@ -103,8 +117,10 @@ export default class Education extends Component {
     return (
 
       <div className="App container">
+        {/*This is the cardiovascular disease infographic*/}
         <img id = "heart" src={AQHeart}  alt="Air Quality Index" height="800" width="500" />
 
+        {/*This is the title of the page and the general AQ info along with the corresponding links*/}
         <h1>Educational Resources</h1> <br></br>
         <h3>General Air Quality Information</h3> 
         <div className='genlinks'>
@@ -113,6 +129,8 @@ export default class Education extends Component {
         </div>
 
         <h4>Learn More!</h4>
+
+        {/*This is the dropdown menu for the user to select the type of information they want to see*/}
         <div className='dropdown'>
         <DropdownButton
         alightCenter
@@ -120,6 +138,7 @@ export default class Education extends Component {
         id="dropdown"
         onSelect={handleSelect}
           >
+                {/*These are the three options the user can select*/}
                 <Dropdown.Item eventKey="option-1">Pollutants</Dropdown.Item>
                 <Dropdown.Item eventKey="option-2">Community</Dropdown.Item>
                 <Dropdown.Item eventKey="option-3">Health</Dropdown.Item>
@@ -127,6 +146,7 @@ export default class Education extends Component {
         </div>
         <br></br>
 
+        {/*This is the section for the pollutant information links*/}
         <div className='links'>
         <h4 id="Common Pollutant Information"></h4>
         <a id="ozone" href="https://www.epa.gov/ozone-pollution"></a> 
@@ -139,6 +159,8 @@ export default class Education extends Component {
         <a id="sulfur" href="https://www.epa.gov/so2-pollution"></a>
         <a id="voc" href="https://www.epa.gov/indoor-air-quality-iaq/volatile-organic-compounds-impact-indoor-air-quality"></a>
 
+
+        {/*This is the section for the community information links*/}
         <h4 id="Community"></h4>
         <a id="schools" href="https://www.epa.gov/iaq-schools/take-action-improve-indoor-air-quality-schools "></a>
         <a id="communities" href="https://www.niehs.nih.gov/research/programs/geh/geh_newsletter/2016/4/spotlight/poor_communities_exposed_to_elevated_air_pollution_levels.cfm"></a>
@@ -147,6 +169,7 @@ export default class Education extends Component {
         <a id="you" href="https://www.pca.state.mn.us/news-and-stories/what-you-can-do-about-air-pollution"></a>
 
         
+        {/*This is the section for the health information links*/}
         <h4 id="Health"></h4>
         <a id="Exposure" href="https://www.who.int/teams/environment-climate-change-and-health/air-quality-and-health/health-impacts/exposure-air-pollution"></a>
         <a id="Indoor" href="https://www.epa.gov/indoor-air-quality-iaq"></a>
@@ -156,6 +179,7 @@ export default class Education extends Component {
         <a id="10" href="https://www.lung.org/blog/air-pollutions-top-10-health-risks"></a>
         </div>
 
+        {/*This is the section for the AQI infographic*/}
         <img id ="range" src={AQRange}  alt="Air Quality Index" height="500" width="750" />
 
         
